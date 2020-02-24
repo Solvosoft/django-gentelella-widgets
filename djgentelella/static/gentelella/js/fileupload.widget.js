69f285517a3617d94ddd60de726bb90c12226da4
Luis Zárate M <luis.zarate@solvosoft.com>
2020-02-24 01:52:46 -0600

    fixed upload file when multiple widget on same page

--- djgentelella/static/gentelella/js/fileupload.widget.js
+++ djgentelella/static/gentelella/js/fileupload.widget.js
@@ -30,38 +30,40 @@
 
             $.each($(this), function(i, e){
                 var $this=$(e),
+                    $parentdiv=$this.closest('.input-group'),
                     upload_url = $this.data('href'),
                     field_name = $this.attr('name'),
-                    div_message = $this.data('message'),
-                    div_process = $this.data('process'),
+                    div_message = $parentdiv.find($this.data('message')),
+                    div_process = $parentdiv.find($this.data('process')),
                     url_done = $this.data('done'),
                     input_token = $this.data('inputtoken'),
-                    fileshow = $this.parent('div').parent('div').find('.fileshow'),
-                    uploadfilecontent = $this.parent('div').parent('div').find('.uploadfilecontent');
+                    fileshow = $parentdiv.find('.fileshow'),
+                    uploadfilecontent = $parentdiv.find('.uploadfilecontent');
 
                $this.attr("required", false);
-               $(div_message).hide();
-               $(fileshow).on('click', function(){
-                    $(uploadfilecontent).toggle();
-                    $(div_message).toggle();
+               div_message.hide();
+               fileshow.on('click', function(){
+                    uploadfilecontent.toggle();
+                    div_message.toggle();
                });
                $this.fileupload({
                   url: upload_url,
                   dataType: "json",
                   maxChunkSize: 100000, // Chunks of 100 kB
                   formData: form_data,
+                  dropZone: $this,
                   add: function(e, data) { // Called before starting upload
 
-                    $(div_message).empty();
+                    div_message.empty();
                     // If this is the second file you're uploading we need to remove the
                     // old upload_id and just keep the csrftoken (which is always first).
                     form_data.splice(1);
                     calculate_md5(data.files[0], 100000);  // Again, chunks of 100 kB
                     data.paramName='file';
                     data.submit();
-                    $(uploadfilecontent).hide();
-                    $(div_message).show();
-                    $(div_message).html(data.files[0].name);
+                    uploadfilecontent.hide();
+                    div_message.show();
+                    div_message.html(data.files[0].name);
                   },
                   chunkdone: function (e, data) { // Called after uploading each chunk
                     if (form_data.length < 2) {
@@ -70,10 +72,10 @@
                       );
                     }
                      var progress = parseInt(data.loaded / data.total * 100.0, 10);
-                    $(div_process).text(  progress + "%");
+                    div_process.text(  progress + "%");
                   }
                 }).bind('fileuploaddone', function (e, data) {
-                    $('input[name="'+input_token+'"]').val(data.result.upload_id);
+                    $parentdiv.find('input[name="'+input_token+'"]').val(data.result.upload_id);
                     $.ajax({
                           type: "POST",
                           url: url_done,
@@ -84,7 +86,7 @@
                           },
                           dataType: "json",
                           success: function(data) {
-                            $(div_process).html(' <i class="fa fa-check"></i>');
+                            div_process.html(' <i class="fa fa-check"></i>');
                           }
                     });
 
