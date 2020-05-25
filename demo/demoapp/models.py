ab0484b71dcb4a1c99c8e3caf885124e92b1bb15
Luis Zárate M <luis.zarate@solvosoft.com>
2020-05-25 00:12:26 -0600

    Catalog model fields

--- demo/demoapp/models.py
+++ demo/demoapp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+from djgentelella.fields.catalog import GTForeignKey, GTManyToManyField, GTOneToOneField
+
 
 class Country(models.Model):
     name = models.CharField(max_length=150)
@@ -17,3 +19,25 @@ class Person(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Catalog(models.Model):
+    key = models.CharField(max_length=150)
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.key +" - "+self.description
+
+
+class WithCatalog(models.Model):
+    mycatalog = GTForeignKey(Catalog, on_delete=models.DO_NOTHING, key_name="key", key_value="Options")
+    countries = GTManyToManyField(Catalog, related_name="countryrel",key_name="key", key_value="countries")
+
+    def __str__(self):
+        return str(self.me)
+
+class OneCatalog(models.Model):
+    me = GTOneToOneField(Catalog, on_delete=models.CASCADE, key_name="key", key_value="countries")
+
+    def __str__(self):
+        return str(self.me)
