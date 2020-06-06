33ef91e76ca803896c3d05a2720043b1c7b3d0e6
Luis Zárate M <luis.zarate@solvosoft.com>
2020-06-06 13:39:20 -0600

    fixed admin import form demoapp

--- demo/demoapp/admin.py
+++ demo/demoapp/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import WithCatalog, Catalog, OneCatalog, Country, Person, Colors
-
+from .models import WithCatalog, Catalog, OneCatalog, Country, Person, Colors, Foo
 
 admin.site.register(Country)
 admin.site.register(Catalog)
@@ -8,4 +7,4 @@ admin.site.register(WithCatalog)
 admin.site.register(OneCatalog)
 admin.site.register(Person)
 admin.site.register(Colors)
-
+admin.site.register(Foo)
