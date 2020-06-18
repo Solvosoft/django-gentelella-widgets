4c74301c96e322b182919d529ed1e5c5bae87193
Luis Zárate M <luis.zarate@solvosoft.com>
2020-06-18 17:53:18 -0600

    fixed datatables reference and include languaje support

--- djgentelella/templatetags/gtsettings.py
+++ djgentelella/templatetags/gtsettings.py
@@ -1,5 +1,7 @@
 from django import template
 from django.utils.safestring import mark_safe
+from django.conf import settings
+from django.utils.translation import get_language
 from djgentelella.utils import get_settings as get_settings_utils
 import uuid
 
@@ -15,3 +17,12 @@ def get_settings(context,  name, **kwargs):
 @register.simple_tag
 def get_random_uuid():
     return str(uuid.uuid4())
+
+
+@register.simple_tag(takes_context=True)
+def get_dataset_translation(context):
+    lang = get_language()
+    if lang:
+        if lang in settings.DATASETS_SUPPORT_LANGUAGES:
+            return settings.DATASETS_SUPPORT_LANGUAGES[lang]
+    return "//cdn.datatables.net/plug-ins/1.10.20/i18n/English.json"
