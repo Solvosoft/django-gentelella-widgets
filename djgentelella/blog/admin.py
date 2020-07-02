a579b80dc1ff1634cdb2247252eff6dbad77b2ed
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2020-07-01 23:53:28 -0600

    Adding small change to pagination links and adding Blog models to admin.py

--- djgentelella/blog/admin.py
+++ djgentelella/blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from djgentelella.blog import models
 
-# Register your models here.
+admin.site.register(models.Entry)
+admin.site.register(models.EntryImage)
+admin.site.register(models.Category)
