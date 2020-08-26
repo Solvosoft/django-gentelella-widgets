fcd5b7e29827c7f3216fd77a7c4587caa82a2ac3
Kejebo <57534905+Kejebo@users.noreply.github.com>
2020-08-26 11:40:36 -0600

    validaciones

--- demo/demoapp/models.py
+++ demo/demoapp/models.py
@@ -125,3 +125,7 @@ class InputMask(models.Model):
     taxid=models.CharField(max_length=25)
     credit_card=models.CharField(max_length=25)
     
+    def __str__(self):
+        return self.phone
+    
+   
