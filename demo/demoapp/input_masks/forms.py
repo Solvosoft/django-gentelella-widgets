f7881a63694ba32101d7af981059aa5bc5a600ef
Kejebo <57534905+Kejebo@users.noreply.github.com>
2020-08-27 09:20:17 -0600

    Update forms.py

--- demo/demoapp/input_masks/forms.py
+++ demo/demoapp/input_masks/forms.py
@@ -20,31 +20,35 @@ class InputMaskForms(forms.ModelForm, CustomForm):
         }
 
     def clean_phone(self):
-        phone=self.cleaned_data["phone"]
-        if len(phone) < 14:
-            raise phone.ValidationError('at least 14 digits')
-        return phone
-    
+        phone = self.cleaned_data["phone"]
+        if phone.find('_') != -1:
+            raise forms.ValidationError('at least 14 digits')
+        else:
+            return phone
+
     def clean_serial_number(self):
-        serial_number=self.cleaned_data["serial_number"]
-        if len(serial_number) < 23:
-            raise serial_number.ValidationError('at least 23 digits')
-        return serial_number
-    
+        serial_number = self.cleaned_data["serial_number"]
+        if serial_number.find('_') != -1:
+            raise forms.ValidationError('at least 23 digits')
+        else:
+            return serial_number
+
     def clean_custom(self):
-        custom=self.cleaned_data["custom"]
-        if len(custom) < 9:
-            raise custom.ValidationError('at least 9 digits')
-        return custom
-    
+        custom = self.cleaned_data["custom"]
+        if custom.find('_') != -1:
+            raise forms.ValidationError('at least 9 digits')
+        else:
+            return custom
+
     def clean_taxid(self):
-        taxid=self.cleaned_data['taxid']
-        if len(taxid) < 11:
-            raise taxid.ValidationError('at least 11 digits')
+        taxid = self.cleaned_data['taxid']
+        if taxid.find('_') != -1:
+            raise forms.ValidationError('at least 11 digits')
         return taxid
-    
+
     def clean_credit_card(self):
-        credit_card=self.cleaned_data["credit_card"]
-        if len(credit_card) < 19:
-            raise credit_card.ValidationError('at least 19 digits')
-        return credit_card
+        credit_card = self.cleaned_data["credit_card"]
+        if credit_card.find('_') != -1:
+            raise forms.ValidationError('at least 19 digits')
+        else:
+            return credit_card
