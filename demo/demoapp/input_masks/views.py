dfa05a60c74a49141d226e2cbd4b80a849ddcad6
Kejebo <57534905+Kejebo@users.noreply.github.com>
2020-08-31 15:27:46 -0600

    remake

--- demo/demoapp/input_masks/views.py
+++ demo/demoapp/input_masks/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from .forms import InputMaskForms, InputMaskFormsClone
+from .forms import InputMaskForms
 from demoapp.models import InputMask
 from datetime import datetime
 
@@ -13,22 +13,6 @@ class InsertMask(CreateView):
     template_name = 'gentelella/input_mask/inputs.html'
     success_url = reverse_lazy('input-mask-list')
 
-    def form_valid(self, form):
-        self.object = form.save(commit=False)
-        phone = self.request.POST.getlist('phone')
-        self.object.phone=phone[0]
-        card=self.object.credit_card.split('_')
-        self.object.credit_card=card[0]
-        self.object.save()
-        return super().form_valid(form)
-    
-    def add_forms(self, request):
-        return render(request, self.template_name, {'form': self.form_class, 'forms': InputMaskFormsClone})
-
-    def get(self, request, *args, **kwargs):
-        return self.add_forms(request)
-
-
 class listMask(ListView):
     model = InputMask
     template_name = 'gentelella/input_mask/view_inputs.html'
@@ -39,9 +23,3 @@ class EditMask(UpdateView):
     form_class = InputMaskForms
     template_name = 'gentelella/input_mask/inputs.html'
     success_url = reverse_lazy('input-mask-list')
-    def form_valid(self, form):
-        self.object = form.save(commit=False)
-        card=self.object.credit_card.split('_')
-        self.object.credit_card=card[0]
-        self.object.save()
-        return super().form_valid(form)
