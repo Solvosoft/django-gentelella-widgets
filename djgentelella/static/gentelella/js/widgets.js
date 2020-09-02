048d3dcb629b0795de76bd873e25b4b4f1b81be4
Kejebo <57534905+Kejebo@users.noreply.github.com>
2020-09-02 09:16:01 -0600

    Update widgets.js

--- djgentelella/static/gentelella/js/widgets.js
+++ djgentelella/static/gentelella/js/widgets.js
@@ -1,3 +1,16 @@
+function decore_select2(data) {
+    // We only really care if there is an element to pull classes from
+    if (!data.element) {
+        return data.text;
+    }
+    var $element = $(data.element);
+    var $wrapper = $('<span></span>');
+    $wrapper.addClass($element[0].className);
+    $wrapper.text(data.text);
+    return $wrapper;
+}
+
+
 document.formset = [];
 document.gtwidgets = {
     Select: function (instance) {
@@ -7,6 +20,9 @@ document.gtwidgets = {
         instance.select2();
     },
     TreeSelect: function (instance) {
+        instance.select2({ templateResult: decore_select2 });
+    },
+    TreeSelectWithAdd: function (instance) {
         instance.select2({ templateResult: decore_select2 });
     },
     CheckboxInput: function(instance){
