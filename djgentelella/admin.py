0d388a62bcebcceb85b343c0c885ce39477ba143
Luis Zárate M <luis.zarate@solvosoft.com>
2020-10-08 21:00:10 -0600

    clean cache DJsettings

--- djgentelella/admin.py
+++ djgentelella/admin.py
@@ -3,15 +3,24 @@ from django.contrib import admin
 # Register your models here.
 from mptt.admin import DraggableMPTTAdmin
 from djgentelella.models import MenuItem, Help, GentelellaSettings, Notification
+from djgentelella.utils import clean_cache
 
 
 class MenuAdmin(DraggableMPTTAdmin):
     filter_horizontal = ['permission']
 
+
 class GentelellaSettingsAdmin(admin.ModelAdmin):
     list_display = ['key', 'value']
     search_fields =  ['key']
     list_editable = ['value']
+    actions = ['clean_settings_cache']
+
+    def clean_settings_cache(self, request, queryset):
+        clean_cache(queryset.values_list('key', flat=True))
+    clean_settings_cache.short_description = "Clean settings cache"
+
+
 
 class NotificationAdmin(admin.ModelAdmin):
     list_display = ['description', 'state', 'message_type']
