c4951133ee31c9727d5f97aaa73343909d62c18f
Luis Zárate M <luis.zarate@solvosoft.com>
2021-03-04 20:06:38 -0600

    fixed upload when pathlib is used

--- djgentelella/editorTinymce/views.py
+++ djgentelella/editorTinymce/views.py
@@ -1,24 +1,27 @@
-from django.http import HttpResponse
+from pathlib import Path
+
 from django.conf import settings
 from django.core.files.storage import default_storage
-import os
-import json
 from django.http.response import JsonResponse
+
 
 def upload(request, folder):
     if 'file' in request.FILES:
         the_file = request.FILES['file']
-        upload_to = getattr(settings,'TINYMCE_UPLOAD_PATH')
-        path = default_storage.save(os.path.join(
-            upload_to+folder, the_file.name), the_file)
+        upload_to = getattr(settings, 'TINYMCE_UPLOAD_PATH')
+        if not isinstance(upload_to, Path):
+            upload_to = Path(upload_to)
+        path = default_storage.save(upload_to / folder / the_file.name, the_file)
         return path
 
+
 def image_upload(request):
-    path = upload(request, '/images')
-    link=path.replace(settings.MEDIA_ROOT, settings.MEDIA_URL)
+    path = upload(request, 'images')
+    link = path.replace(str(settings.MEDIA_ROOT), settings.MEDIA_URL).replace("//", "/")
     return JsonResponse({'link': link})
 
+
 def video_upload(request):
-   path = upload(request, '/videos')
-   link=path.replace(settings.MEDIA_ROOT, settings.MEDIA_URL)
-   return JsonResponse({'link': link})
+    path = upload(request, 'videos')
+    link = path.replace(str(settings.MEDIA_ROOT), settings.MEDIA_URL).replace("//", "/")
+    return JsonResponse({'link': link})
