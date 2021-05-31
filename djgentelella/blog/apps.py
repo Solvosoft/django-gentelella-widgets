80c7be088745165b1425168bceed9ab73fac1136
Kejebo <kenjen041@gmail.com>
2021-05-31 15:45:26 -0600

    updated the blog app name

--- djgentelella/blog/apps.py
+++ djgentelella/blog/apps.py
@@ -1,5 +1,7 @@
+from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
 class BlogConfig(AppConfig):
-    name = 'apps.blog'
+    default_auto_field = 'django.db.models.AutoField'
+    name = 'djgentelella.blog'
