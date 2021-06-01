0d504587406330b93facdea6ad3031a9503ba945
Marcela González Zúñiga <marcegz31@gmail.com>
2021-06-01 17:58:02 -0600

    PermissionCategoryManagement form and global variables were included

---
+++ djgentelella/permission_management/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from forms.forms import GTForm
+from djgentelella.settings import Group, User
+from djgentelella.models import PermissionsCategoryManagement
+
+class PermCategoryManagementForm(GTForm, forms.Form):
+    type = forms.ChoiceField(choices=((1, 'User'), (2, 'Group')), required=True)
+    user = forms.ModelChoiceField(queryset=User.objects.all(), required=False)
+    group = forms.ModelChoiceField(queryset=Group.objects.all(), required=False)
+    permissions = forms.ModelMultipleChoiceField(queryset=PermissionsCategoryManagement.objects.all())
