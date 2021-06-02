74e37aed5ff55f0d8f7847ce9fff1faf6a806b3d
William Alexander <william.al1379@gmail.com>
2021-06-02 17:29:12 -0600

    Fix validation errors

--- djgentelella/permission_management/forms.py
+++ djgentelella/permission_management/forms.py
@@ -9,15 +9,20 @@ class PermCategoryManagementForm(GTForm, forms.Form):
     type = forms.ChoiceField(choices=((1, 'User'), (2, 'Group')), required=True)
     user = forms.ModelChoiceField(queryset=User.objects.all(), required=False)
     group = forms.ModelChoiceField(queryset=Group.objects.all(), required=False)
-    permissions = forms.ModelMultipleChoiceField(queryset=PermissionsCategoryManagement.objects.all())
+    permissions = forms.ModelMultipleChoiceField(queryset=PermissionsCategoryManagement.objects.all(), required=False)
 
-    def clean(self):
+    def clean_user(self):
         type = self.cleaned_data.get("type")
         user = self.cleaned_data.get("user")
-        group = self.cleaned_data.get("group")
         if type == 1:
             if user == "" or user is None:
                 raise forms.ValidationError("Select "+USER_MODEL_BASE.split('.')[1].lower())
-        else:
+        return user
+    
+    def clean_group(self):
+        type = self.cleaned_data.get("type")
+        group = self.cleaned_data.get("group")
+        if type ==2:
             if group == "" or group is None:
                 raise forms.ValidationError("Select "+GROUP_MODEL_BASE.split('.')[1].lower())
+        return group
