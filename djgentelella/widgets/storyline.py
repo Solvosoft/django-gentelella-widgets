a576bbe56afb697690c656d3c215b95f4217992d
ricardoalfaro <rialfaro98@gmail.com>
2022-01-20 19:16:41 -0600

    storylineJS widget implemented, with respective example,still missing multi instance functionality and documentation

--- djgentelella/widgets/storyline.py
+++ djgentelella/widgets/storyline.py
@@ -1,21 +1,19 @@
+import json
+
 from .core import TextInput, update_kwargs
 
 
 class UrlStoryLineInput(TextInput):
     template_name = 'gentelella/widgets/storyline.html'
 
-    def __init__(self, attrs=None):
-        attrs = update_kwargs(attrs, self.__class__.__name__,
-                              base_class='form-control')
+    def __init__(self, csv=None, options=None, attrs=None):
+        self.csv = csv
+        self.options = options
+        attrs = update_kwargs(attrs, self.__class__.__name__,"")
         super(UrlStoryLineInput, self).__init__(attrs=attrs, extraskwargs=False)
 
-    def render(self, name, value, attrs=None, renderer=None):
-        self.value = value
-        return super().render(name, value, attrs=attrs, renderer=renderer )
-
-    def build_attrs(self, base_attrs, extra_attrs=None):
-        """Build an attribute dictionary."""
-        attrs = super().build_attrs(base_attrs, extra_attrs=extra_attrs)
-        if self.value is not None:
-            attrs['data-url'] = self.value
-        return attrs
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs=attrs)
+        context['options'] = self.options
+        context['csv'] = json.dumps(self.csv)
+        return context
