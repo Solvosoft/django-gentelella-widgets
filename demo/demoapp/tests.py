4cbf5f3ca9af926c367334e61b6a3a937da33b10
mariovaargas <mariovargasc97@gmail.com>
2022-01-26 07:35:18 -0600

    Added gigapixel storymap options refectoring

--- demo/demoapp/tests.py
+++ demo/demoapp/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.chrome.webdriver import WebDriver
 
 # Create your tests here.
+
+
+class StoryMapWithSeleniumTestCase(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUp(cls):
+        super().setUp()
+        cls.selenium = WebDriver(executable_path='/Users/mariovargas/Desktop/chromedriver')
+        cls.live_server_url = 'http://127.0.0.1:8000/'
+
+    @classmethod
+    def tearDown(cls):
+        cls.selenium.quit()
+        super().tearDown()
+
+    def test_display_gigapixel_storymap(self):
+        selenium = self.selenium
+        selenium.get(self.live_server_url)
+        # Find storymap obj
+        assert 'id_storymap' in self.selenium.page_source
+
