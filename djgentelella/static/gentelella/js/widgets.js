f2e45e563b8cd8b70190893053239f9878452f1d
mariovaargas <mariovargasc97@gmail.com>
2022-01-26 10:06:05 -0600

    Made some optimization to the storymaps widgets

--- djgentelella/static/gentelella/js/widgets.js
+++ djgentelella/static/gentelella/js/widgets.js
@@ -241,42 +241,10 @@ document.gtwidgets = {
         })
     },
     GigaPixelStoryMapInput: function (instance) {
-        instance.each(function(index, element) {
-            var instanceid = document.getElementById(element.id).id;
-            var data_url = element.getAttribute('data-url');
-            var storymap_options = element.getAttribute('storymap_options');
-
-            var storymap = new VCO.StoryMap(instanceid, data_url, storymap_options);
-
-            var e = $(window).height(),
-            t = $(`#${instanceid}`);
-            t.height(e - 20);
-
-            $(window).resize(function() {
-                e = $(window).height();
-                t.height(e - 20);
-                storymap.updateDisplay();
-            })
-        });
-
+        build_gigapixel_storymap(instance);
     },
     MapBasedStoryMapInput: function (instance) {
-        instance.each(function(index, element) {
-            var instanceid = document.getElementById(element.id).id;
-            var data_url = element.getAttribute('data-url');
-
-            var storymap = new KLStoryMap.StoryMap(instanceid, data_url);
-
-            var e = $(window).height(),
-            t = $(`#${instanceid}`);
-            t.height(e - 20);
-
-            $(window).resize(function() {
-                e = $(window).height();
-                t.height(e - 20);
-                storymap.updateDisplay();
-            })
-        });
+        build_mapbased_storymap(instance);
     }
 
 }
