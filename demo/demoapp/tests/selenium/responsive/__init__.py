a5eee52ce569b37bbdc8ba371a69166411c97470
andrey <andex619@gmail.com>
2022-09-27 10:10:25 -0600

    creacion rama

--- demo/demoapp/tests/selenium/responsive/__init__.py
+++ demo/demoapp/tests/selenium/responsive/__init__.py
@@ -2,14 +2,10 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import reverse
 #from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.firefox.options import Options
 from pathlib import Path
 from django.conf import settings
 import shutil
 from Screenshot import Screenshot
-import time
-from selenium.webdriver.support.wait import WebDriverWait
-
 
 class ScreenshotSeleniumTest(StaticLiveServerTestCase):
     @classmethod
@@ -20,11 +16,17 @@ class ScreenshotSeleniumTest(StaticLiveServerTestCase):
         cls.ob = Screenshot.Screenshot()
         cls.selenium = WebDriver()
         cls.selenium.implicitly_wait(cls.timeout)
-        cls.dir = Path(settings.BASE_DIR) / 'tmp/1920×1080/'
-        cls.server_thread.port= 8012
-        if not cls.dir.exists():
-            cls.dir.mkdir()
+        cls.width = 600
+        cls.height = 800
+        cls.tmp = Path(settings.BASE_DIR) / 'tmp'
+        cls.folder = '%s/%dx%d' % (cls.tmp, cls.width, cls.height)
+        cls.dir = Path(settings.BASE_DIR) / cls.folder
+        cls.server_thread.port = 8012
+        if cls.dir.exists():
+            shutil.rmtree(str(cls.dir.absolute().resolve()))
 
+        cls.tmp.mkdir()
+        cls.dir.mkdir()
 
     def test_snaptshot(self):
         name = 'home'
@@ -32,7 +34,8 @@ class ScreenshotSeleniumTest(StaticLiveServerTestCase):
         self.selenium.get(url)
         print("Headless Firefox Initialized")
         print(self.selenium.get_window_size())
-        self.selenium.set_window_size(1920, 1080)
+
+        self.selenium.set_window_size(self.width, self.height)
         size = self.selenium.get_window_size()
         print("Window size: width = {}px, height = {}px".format(size["width"], size["height"]))
         x_Page='PAGE_SCREENSHOT_%s.png'% name
@@ -40,7 +43,7 @@ class ScreenshotSeleniumTest(StaticLiveServerTestCase):
 
         self.selenium.save_screenshot(str(Path(self.dir / x_Page).absolute().resolve()))
         self.selenium.save_full_page_screenshot(str(Path(self.dir / x_Full_Page).absolute().resolve()))
-        self.selenium.quit()
+
     @classmethod
     def tearDownClass(cls):
         cls.selenium.quit()
