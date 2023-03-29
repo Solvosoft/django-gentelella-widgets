2ec01cc77b8db4fa2d25a0e4c3d7aa620d4186fc
Daniel Loaiza <danieloaiza@live.com>
2023-03-29 17:26:10 -0600

    Added API Data Processing, multiple items implementation bugged

--- demo/demoapp/select2box/forms.py
+++ demo/demoapp/select2box/forms.py
@@ -6,4 +6,5 @@ from djgentelella.widgets.core import Select2Box
 
 class dataOptions(GTForm):
     mydata = forms.ChoiceField(widget=Select2Box, choices=[[1, "primero"], [2, "segundo"], [3, "tercero"]])
-    #mymenu = forms.ModelMultipleChoiceField(widget=Select2Box(attrs={'data-url':'www.testtext.com'}), queryset=MenuItem.objects.all())
+    mymenu = forms.ModelMultipleChoiceField(widget=Select2Box(attrs={'data-url':'https://pokeapi.co/api/v2/pokemon/?limit=10'}), queryset=MenuItem.objects.all())
+    mydataTest = forms.ChoiceField(widget=Select2Box, choices=[[1, "primero"], [2, "segundo"], [3, "tercero"]])
--- djgentelella/static/gentelella/js/base/select2box.js
+++ djgentelella/static/gentelella/js/base/select2box.js
@@ -4,21 +4,49 @@ function get_select2box(instance){
     //Template for the options created
     'opt_template': '<option value="new_value"> new_display </option>',
     //Instance of the parent container
-    'container': $(instance).closest('.select2box_container'),
+    'container': null,
     'options':null,
     'selected_temp': null,
     'init': function(){
                 let parent = this.container[0] //DOM Container
+                console.log(this.container)
+                let url = this.container.find('.select2box_available').data('url') //undefined or data
+                if(url !== undefined){
+                    this.get_api_data(url)
+                }
                 this.options = parent.getElementsByClassName("select2box_options")[0]
                 this.selected_temp = parent.getElementsByClassName("select2box_available form-control")[0]
-                this.initialize_elem_func();
+                this.initialize_elem_func(this);
                 this.disable_property(); //Checks if data exists in each select HTML element
-                /*
-                  this.container.find('.select2box_options').data('url') === undefined or data
-                */
             },
-    'initialize_elem_func':function(){
-                        this.container.find('.add_selection').on("click", () => this.selected_add());
+    //Fetch the data from an API and inserts it into the options
+    'get_api_data': function(url){
+                        let value_dictionary = {};
+                        fetch(url).then(response => response.json()).then(data => {
+                            let key_to_use = Object.keys(data.results[0])[0];
+                            for(let e in data.results ) {
+                                //console.log(e, Object.keys(data.results[e])[0]);
+                                value_dictionary[e] = data.results[e][key_to_use]
+                            }
+                            this.insert_api_data(value_dictionary);
+                          })
+                          .catch(error => {
+                            //console.error('Error:', error);
+                            console.log("Can't get the API Data, verify the url")
+                          });
+                        console.log(value_dictionary)
+    },
+    'insert_api_data': function(value_dictionary){
+                        let temp_opt_format = this.opt_template;
+                        let temp_ref = this.container.find('.select2box_options');
+                        this.remove_all_data();
+                        for(let e in value_dictionary ) {
+                            let temp_format = temp_opt_format.replace('new_value', e).replace('new_display', value_dictionary[e])
+                            temp_ref.append(temp_format)
+                        }
+    },
+    'initialize_elem_func':function(obj){
+                        this.container.find('.add_selection').on("click", () => obj.selected_add(obj));
                         this.container.find('.select2box_options').on("dblclick", () => this.selected_add());
                         this.container.find('.return_selected').on("click", () => this.remove_selected());
                         this.container.find('.select2box_available').on("dblclick", () => this.remove_selected());
@@ -33,17 +61,19 @@ function get_select2box(instance){
                                                     this.container.find('#display_select2box')[0].value = "";
                         })
                      },
+
     //Add selected options to the selected container
-    'selected_add': function(){
-                        let temp_ref = this.container.find('.select2box_available');
-                        let temp_opt_format = this.opt_template;
-                        let opt = this.options.querySelectorAll(':checked')
+    'selected_add': function(obj){
+                        console.log(obj)
+                        let temp_ref = obj.container.find('.select2box_available');
+                        let temp_opt_format = obj.opt_template;
+                        let opt = obj.options.querySelectorAll(':checked')
                         opt.forEach((e) => {
                           let temp_format = temp_opt_format.replace('new_value', e.value).replace('new_display', e.innerHTML)
                           temp_ref.append(temp_format)
                           e.remove()
                         });
-                        this.disable_property();
+                        obj.disable_property();
                     },
     //Add selected options to the available container
     'remove_selected': function(){
@@ -80,12 +110,12 @@ function get_select2box(instance){
     },
     //Disables buttons depending on the data in them
     'disable_property': function() {
-                        let nodes_available = this.options.querySelectorAll('option').length;
-                        let nodes_selected = this.selected_temp.querySelectorAll('option').length;
-                        this.container.find('.all_to_available')[0].disabled = (nodes_selected === 0);
-                        this.container.find('.all_to_selected')[0].disabled = (nodes_available === 0);
-                        this.container.find('.add_selection')[0].disabled = (nodes_available === 0);
-                        this.container.find('.return_selected')[0].disabled = (nodes_selected === 0);
+                        let nodes_available = this.options.querySelectorAll('option').length === 0;
+                        let nodes_selected = this.selected_temp.querySelectorAll('option').length === 0;
+                        this.container.find('.all_to_available')[0].disabled = (nodes_selected);
+                        this.container.find('.all_to_selected')[0].disabled = (nodes_available);
+                        this.container.find('.add_selection')[0].disabled = (nodes_available);
+                        this.container.find('.return_selected')[0].disabled = (nodes_selected);
 
     },
     //Filters data inside the select_options HTML element
@@ -111,16 +141,24 @@ function get_select2box(instance){
                         let temp_ref = this.container.find('.select2box_options')
                         if (value !== "" && display !== ""){
                             let temp_format = temp_opt_format.replace('new_value', value).replace('new_display', display);
-                            console.log(temp_format)
                             temp_ref.append(temp_format);
                             $('#select2box_modal').modal('hide');
                         }
                         else{
-                            this.container.find('#display_select2box')[0]
+                            alert("Invalid data provided")
                         }
     },
     }
-    select2box.init()
+    let all_instances = $(instance).closest('.select2box_container')
+    delete all_instances.prevObject
+    delete all_instances.length
+    console.log(all_instances)
+    container_values = Object.keys(all_instances)
+    container_values.forEach((e) => {
+        let _ = select2box
+        _.container = $(all_instances[e])
+        _.init()
+    })
     return select2box
 
 }
