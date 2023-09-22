551b0d775f947eeac624903a7fcd384dbafce607
FiorellaBonilla <fiorella.bonilla.gonzalez@est.una.ac.cr>
2023-09-22 09:49:46 -0600

    add function post migrate

--- djgentelella/apps.py
+++ djgentelella/apps.py
@@ -1,19 +1,16 @@
 from django.apps import AppConfig
-import logging  # Importa el módulo de registro
+
+from django.db.models.signals import post_migrate
+
+def my_callback(sender, **kwargs):
+
+
+    from django.core import management
+    management.call_command('show_permissions')
 
 class DjgentelellaConfig(AppConfig):
     default_auto_field = 'django.db.models.AutoField'
     name = 'djgentelella'
 
     def ready(self):
-        super(DjgentelellaConfig, self).ready()
-
-        # Configura el registro
-        logger = logging.getLogger(__name__)
-
-        # Agrega un mensaje de registro para verificar que el método ready se está ejecutando
-        logger.info("La aplicación DjgentelellaConfig está lista.")
-
-        # Importa y ejecuta tu comando personalizado aquí
-        from django.core import management
-        management.call_command('show_permissions')
+        post_migrate.connect(my_callback, sender=self)
