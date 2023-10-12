2ffee368fc59646f55a5810d456d41b269f8d1e6
Luis Zárate Montero <luis.zarate@solvosoft.com>
2023-10-12 09:25:51 -0600

    Fixed chunked uplod widget in forms fields, adding new representation of the file

--- djgentelella/widgets/files.py
+++ djgentelella/widgets/files.py
@@ -1,5 +1,9 @@
+import json
+from pathlib import Path
+
 from django.forms import FileInput
 from django.urls import reverse_lazy
+from django.utils.safestring import mark_safe
 
 from djgentelella.models import ChunkedUpload
 from djgentelella.widgets.core import update_kwargs
@@ -22,17 +26,39 @@ class FileChunkedUpload(FileInput):
 
     def format_value(self, value):
         """File input never renders a value."""
-        return value
+        if value:
+            name = Path(value.name).name
+            self.value = mark_safe(
+                '{"name": "%(name)s", "display_name": "%(display_name)s", "url": "%(url)s" }' % {
+                    'name': value.name,
+                    'display_name': name,
+                    'url': value.url})
+            return self.value
+        return ''
+
+    def parse_value(self, value):
+        dev = None
+        try:
+            dev = json.loads(value)
+            if not ('url' in dev or 'token' in dev or 'actions' in dev):
+                dev = None
+        except Exception as e:
+            pass
+        return dev
 
     def value_from_datadict(self, data, files, name):
         dev = None
-        token = data.get(name)
-        if token == '0':
-            return False
-        tmpupload = ChunkedUpload.objects.filter(upload_id=token).first()
-        if tmpupload:
-            dev = tmpupload.get_uploaded_file()
-            tmpupload.delete()
+
+        token = self.parse_value(data.get(name))
+        if token:
+            if 'actions' in token and token['actions'] == 'delete':
+                return False
+            if 'token' in token:
+                tmpupload = ChunkedUpload.objects.filter(
+                    upload_id=token['token']).first()
+                if tmpupload:
+                    dev = tmpupload.get_uploaded_file()
+                    tmpupload.delete()
         return dev
 
     def value_omitted_from_data(self, data, files, name):
