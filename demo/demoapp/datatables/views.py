dd845be4304b870143a737f2396f5c6a543878bf
Mauri4302 <54749838+Mauri4302@users.noreply.github.com>
2025-02-06 08:37:00 -0600

    Implementing cardtable to display information in cards and have a different visualization than datatables.

--- demo/demoapp/datatables/views.py
+++ demo/demoapp/datatables/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def datatableViewExample(response):
     return render(response, 'gentelella/datatables/datatables.html')
+
+# CardTable
+def cardtableViewExample(response):
+    return render(response, 'gentelella/cardtables/cardtables.html')
