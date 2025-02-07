bd53def690cbb03223b2c6f14c6dade04cbeb5a4
liwbh <liwbarqueroh@gmail.com>
2025-02-07 17:41:59 -0600

    fix input

--- demo/demoapp/digital_signature/forms.py
+++ demo/demoapp/digital_signature/forms.py
@@ -1,17 +1,16 @@
 from django import forms
 from django.conf import settings
 
-from djgentelella.models import ChunkedUpload
+from demoapp.models import DigitalSignature
 from djgentelella.forms.forms import GTForm
 from djgentelella.widgets.digital_signature import DigitalSignatureInput
 
 class DigitalSignatureForm(GTForm, forms.ModelForm):
 
     class Meta:
-        model = ChunkedUpload
-        fields = ['file', 'filename']
+        model = DigitalSignature
+        fields = ['file']
         widgets = {
-            'filename': forms.HiddenInput,
             'file': DigitalSignatureInput(
                 ws_url=settings.FIRMADOR_WS
             )
--- djgentelella/widgets/digital_signature.py
+++ djgentelella/widgets/digital_signature.py
@@ -1,21 +1,18 @@
 import uuid
-from django.forms import FileInput
+from django.forms import HiddenInput
 from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
 from djgentelella.widgets.core import update_kwargs
 
 
-class DigitalSignatureInput(FileInput):
+class DigitalSignatureInput(HiddenInput):
     template_name = 'gentelella/widgets/digital_signature.html'
-    input_type = 'file'
+    input_type = 'hidden'
 
-    def __init__(self, attrs=None,  extraskwargs=True, ws_url=None):
+    def __init__(self, attrs=None, extraskwargs=True, ws_url=None):
         attrs = attrs or {}
-
         attrs['data-ws-url'] = ws_url
-
         if extraskwargs:
             attrs = update_kwargs(
                 attrs, self.__class__.__name__,)
         super().__init__(attrs)
-
