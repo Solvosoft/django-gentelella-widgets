739df1c9c09cf3546da1ba4ae159ce7fda17665c
liwbh <liwbarqueroh@gmail.com>
2025-03-05 09:06:13 -0600

    added dynamic logger

--- demo/demo/asgi.py
+++ demo/demo/asgi.py
@@ -12,5 +12,22 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 # application = get_asgi_application()
 
 #  Para usar digital_signature
-from djgentelella.firmador_digital.config.asgi_config import AsgiConfig
-application = AsgiConfig("demo.settings").application
+# from djgentelella.firmador_digital.config.asgi_config import AsgiConfig
+# application = AsgiConfig("demo.settings").application
+
+
+import os
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from djgentelella.firmador_digital.config.websocket_urls import websocket_urlpatterns
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demo.settings")
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+    }
+)
