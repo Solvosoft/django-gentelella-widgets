bad04cdad0840ff711af57663e2ee300acde85ce
liwbh <liwbarqueroh@gmail.com>
2025-08-20 17:15:03 -0600

    Remove compiled .mo files from version control and resolved observations

--- demo/demoapp/history/forms.py
+++ demo/demoapp/history/forms.py
@@ -7,7 +7,7 @@ from django.utils.translation import gettext_lazy as _
 class HistoryFilterForm(GTForm, forms.Form):
     category = forms.ChoiceField(
         choices=[
-            ("--------", "General"),
+            ("--------", _("All")),
             ("demoapp.customer", _("Customer")),
             ("djgentelella.trash", _("Trash")),
             # add more categories here
