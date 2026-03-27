44a4daa70c804e5c7f3261ec6bca534f23e7b0d8
Wilfredo Barquero Herrera <wilfredo.barquero.herrera@est.una.ac.cr>
2026-03-27 13:06:57 -0600

    fixed text corrector

--- djgentelella/widgets/wysiwyg.py
+++ djgentelella/widgets/wysiwyg.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.forms import Textarea
 from django.urls import reverse_lazy
+from django.utils.translation import get_language
 
 from djgentelella.widgets.core import update_kwargs
 
@@ -8,6 +10,9 @@ class TextareaWysiwyg(Textarea):
     template_name = 'gentelella/widgets/wysiwyg.html'
 
     def __init__(self, attrs=None, extraskwargs=True):
+        attrs = attrs or {}
+        attrs.setdefault("data-option-spellcheck", "true")
+        attrs.setdefault("data-option-lang", get_language() or settings.LANGUAGE_CODE)
         if extraskwargs:
             attrs = update_kwargs(attrs, self.__class__.__name__,
                                   base_class='wysiwyg form-control')
