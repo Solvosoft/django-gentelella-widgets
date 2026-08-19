d37dd915624abb34a41d19a79d17c9b0e5362827
Wilfredo Barquero Herrera <wilfredo.barquero.herrera@est.una.ac.cr>
2026-08-19 17:06:14 -0600

    Fixed calendar

--- demo/demoapp/models.py
+++ demo/demoapp/models.py
@@ -228,6 +228,8 @@ class Event(models.Model):
     title = models.CharField(max_length=255, null=True, blank=True)
     start = models.DateTimeField(null=True, blank=True)
     end = models.DateTimeField(null=True, blank=True)
+    color = models.CharField(max_length=20, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.title
