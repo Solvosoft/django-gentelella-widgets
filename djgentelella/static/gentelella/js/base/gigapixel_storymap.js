5dadccbe5f88b5bf6e7dd9792c8f960e941db89c
Kejebo <kenjen041@gmail.com>
2026-08-19 16:38:23 -0600

    fixed storymap widget

--- djgentelella/static/gentelella/js/base/gigapixel_storymap.js
+++ djgentelella/static/gentelella/js/base/gigapixel_storymap.js
@@ -2,7 +2,12 @@ function build_gigapixel_storymap(instance) {
     instance.each(function(index, element) {
         var instanceid = document.getElementById(element.id).id;
         var data_url = element.getAttribute('data-url');
-        var storymap_options = element.getAttribute('storymap_options');
+        var storymap_options_str = element.getAttribute('storymap_options');
+        var storymap_options = storymap_options_str ? JSON.parse(storymap_options_str) : {};
+
+        if (window.L_storymap) {
+            window.L = window.L_storymap;
+        }
 
         var storymap = new VCO.StoryMap(instanceid, data_url, storymap_options);
 
